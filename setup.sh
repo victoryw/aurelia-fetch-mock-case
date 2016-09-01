9acdce81de40e2ecc6da6fd3829fc6b5bda77222
tianyawy <tianya_wy@hotmail.com>
2016-09-02 02:03:25 +0800

    [victoryw] import the webpack

--- setup.sh
+++ setup.sh
@@ -2,5 +2,10 @@ npm init -yes
 npm install karma --save-dev
 npm install karma-jasmine karma-chrome-launcher --save-dev
 npm install --save-dev jasmine
-node_modules/karma/bin/karma init
+npm install karma-sourcemap-loader --save-dev
+npm install webpack --save-dev
 
+if [ ! -f ./karma.conf.js ]
+then
+	node_modules/karma/bin/karma init
+fi
